810bc6b8dca70c9e309e7718d29ae5badafcfa5a
Dario <dgarciamoya@WS-5NFZH42.ggs-hh.net>
2015-12-17 08:57:55 +0100

    add markdown translator

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,17 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        markdown: {
+            all: {
+                files: [{
+                    expand: true,
+                    src: ['README.md'],
+                    dest: 'markdown/',
+                    ext: '.html'
+                }]
+            }
+        },
+
         jsdoc : {
             dist : {
                 src: ['diy/*.js'],
@@ -34,7 +45,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-jscs');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-markdown');
 
+    grunt.registerTask('mark', ['markdown']);
     grunt.registerTask('doc', ['jsdoc']);
     grunt.registerTask('qa', [
         'jshint',
